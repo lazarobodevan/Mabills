fc330db30773f5c25204d425d4293abd50913b12
lazarobodevan <lazarobodevan@gmail.com>
2023-03-03 20:56:37 -0300

    feat(front): pagina de login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { InputFieldComponent } from './components/input-field/input-field.component';
+import { ActionButtonComponent } from './components/action-button/action-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    InputFieldComponent
+    InputFieldComponent,
+    ActionButtonComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/components/action-button/action-button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-action-button',
+  templateUrl: './action-button.component.html',
+  styleUrls: ['./action-button.component.css']
+})
+export class ActionButtonComponent {
+  @Input() text: string = "";
+  @Input() width: string = "";
+}
