4217ddf4904cd935af3f8cfba290d3d8226c673c
Lázaro Bodevan <lazarobodevan@gmail.com>
2023-03-14 17:37:08 -0300

    feat(front): página de dashboard

--- frontend/src/app/components/card/card.component.ts
+++ frontend/src/app/components/card/card.component.ts
@@ -10,4 +10,24 @@ export class CardComponent {
   @Input() text: string = '';
   @Input() value: number = 0;
   @Input() backgroundColor: string = 'blue';
+  @Input() variation: number = 0;
+
+  defineVariation(){
+    if(this.variation > 0){
+      return {
+        class: 'positive',
+        icon: 'arrow_drop_up'
+      }
+    }else if(this.variation < 0){
+      return {
+        class: 'negative',
+        icon: 'arrow_drop_down'
+      }
+    }else{
+      return {
+        class: 'neutral',
+        icon: 'remove'
+      }
+    }
+  }
 }
--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,102 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  @ViewChild("chartCanvas", {static: true}) element!: ElementRef;
+  @ViewChild("income", {static: true}) incomeChart!: ElementRef;
+  @ViewChild("expense", {static: true}) expenseChart!: ElementRef;
+
+  ngOnInit(){
+    Chart.defaults.borderColor = "rgba(255, 255, 255, 0.22)";
+    Chart.defaults.color = "#FFFFFF"
+    this.generateIncomeExpesesChart();
+    this.generateIncomeChart();
+    this.generateExpenseChart();
+  }
+
+  generateIncomeExpesesChart(){
+    new Chart(this.element.nativeElement,{
+      type:'line',
+      options: {
+        responsive: true,
+        interaction: {
+          intersect: false,
+          mode: 'index',
+        },
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Receita x Despesas'
+          },
+        }
+      },
+      data: {// values on X-Axis
+        labels: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho'],
+	       datasets: [
+          {
+            label: 'Receita',
+            data: [1467,276, 1157, 2229, 1092,
+								 1574, 1573, 1576],
+            borderColor: 'green',
+            backgroundColor: 'green'
+          },
+          {
+            label: 'Despesa',
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+            borderColor: 'red',
+            backgroundColor: 'red'
+          },
+        ]
+      },
+    });
+  }
+
+  generateIncomeChart(){
+    new Chart(this.incomeChart.nativeElement,{
+      type:'doughnut',
+      options:{
+        plugins: {
+          title: {
+            display: true,
+            text: 'Fonte de receita'
+          },
+        }
+      },
+      data: {// values on X-Axis
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+          },
+        ]
+      },
+    })
+  }
+  generateExpenseChart(){
+    new Chart(this.expenseChart.nativeElement,{
+      type:'doughnut',
+      options:{
+        plugins: {
+          title: {
+            display: true,
+            text: 'Fonte de despesa'
+          },
+        }
+      },
+      data: {// values on X-Axis
+	       datasets: [
+          {
+            label: "Sales",
+            data: ['467','576', '572', '79', '92',
+								 '574', '573', '576'],
+          },
+        ]
+      },
+    })
+  }
 
 }
