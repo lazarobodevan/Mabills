75277c06d932ad0478c09a74547db85a070976ab
Lázaro Bodevan <lazarobodevan@gmail.com>
2023-04-11 21:02:55 -0300

    feat(front): atualizando usuário

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { IUser } from 'src/app/interfaces/IUser';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProfileComponent {
+  loggedUser = {} as IUser;
 
+  constructor(private userService:UserService, public ref:ChangeDetectorRef){
+    this.getLoggedUser();
+  }
+
+  getLoggedUser(){
+    this.userService.getLoggedUser().subscribe(response=>{
+      this.loggedUser = response;
+      this.ref.detectChanges();
+    });
+  }
+
+  setName(event:string){
+    this.loggedUser.name = event;
+  }
+
+  setEmail(event:string){
+    this.loggedUser.email = event;
+  }
+
+  setPassword(event:string){
+    this.loggedUser.password = event;
+  }
+
+  updateUser(){
+    this.userService.updateUser(this.loggedUser).subscribe(response =>{
+      this.getLoggedUser();
+      this.ref.detectChanges();
+    })
+  }
 }
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../interfaces/IUser';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private headers = new HttpHeaders({
+    'Content-type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('token')}`
+  });
+
+  constructor(private http: HttpClient) { }
+
+  getLoggedUser():Observable<IUser>{
+    return this.http.get<IUser>(`${environment.API}user`, {headers: this.headers});
+  }
+
+  updateUser(user:IUser):Observable<IUser>{
+    return this.http.put<IUser>(`${environment.API}users`,{
+      name: user.name,
+      password: user.password,
+      email: user.email
+    },{headers: this.headers});
+  }
 }
