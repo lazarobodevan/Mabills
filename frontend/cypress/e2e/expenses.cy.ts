1ccc3997be33c601a683aefd3ca63a168628fafb
Lázaro Bodevan <lazarobodevan@gmail.com>
2023-05-03 21:07:43 -0300

    test(front): testando edição e deleção de transação

--- frontend/cypress/e2e/expenses.cy.ts
+++ frontend/cypress/e2e/expenses.cy.ts
@@ -1,3 +1,5 @@
+import * as moment from 'moment';
+
 describe("#TRANSACTIONS", ()=>{
 
     before(()=>{
@@ -49,6 +51,56 @@ describe("#TRANSACTIONS", ()=>{
         cy.getByTestId('transaction-value').should('have.text','R$ 123');
         cy.getByTestId('transaction-date').should("contain","02/05/2023");
         cy.getByTestId('transaction-isPaid').should("contain","-");
+    });
 
+    it('Should load the selected transaction information in the modal', ()=>{
+        cy.get('[type="radio"]').first().check();
+        cy.getByTestId("update-button").click();
+        cy.getByTestId('name-input').invoke("attr","ng-reflect-input-value").should("contain",'transaction');
+        cy.getByTestId('type-select').should("have.value", "INCOME")
+        cy.getByTestId('category-select').find(':selected').contains("category test");
+        cy.getByTestId('date-input').children().children().invoke("attr","ng-reflect-model").should('contain','2023-05-02');
+        
+        cy.getByTestId('value-input').invoke("attr","ng-reflect-input-value").should("contain",'123');
+    });
+
+    it("Should update the selected transaction", ()=>{
+        //Adding category
+        cy.visit('/categories');
+        cy.getByTestId("add-button").click();
+        cy.getByTestId("name-input").type("category2");
+        cy.getByTestId("color-input").click();
+        cy.get("[type='color']").eq(1).invoke('val', '#ffffff').trigger('input')
+        cy.getByTestId("conclude-button").click();
+
+        //Adding transaction
+        cy.visit('/expenses')
+        cy.get('[type="radio"]').first().check();
+        cy.getByTestId("update-button").click();
+        cy.getByTestId('name-input').clear().type("transaction2");
+        cy.getByTestId('type-select').select(1).should("have.value", "EXPENSE")
+        cy.getByTestId('category-select').select(2).find(':selected').contains("category2");
+        cy.getByTestId('date-input').type("2023-05-03");
+        cy.getByTestId('value-input').clear().type('321');
+        cy.getByTestId('isPaid-checkbox').check();
+        cy.getByTestId('conclude-button').click()
+
+        //Verifying if it is displayed after added
+        cy.getByTestId('transaction-name').should("have.text", "transaction2");
+        cy.getByTestId('transaction-type').should("contain", "Despesa");
+        cy.getByTestId('transaction-color').should("have.css",'background-color','rgb(255, 255, 255)');
+        cy.getByTestId('transaction-category-name').should("have.text", "category2");
+        cy.getByTestId('transaction-value').should('have.text','R$ 321');
+        cy.getByTestId('transaction-date').should("contain","03/05/2023");
+        cy.getByTestId('transaction-isPaid').should("contain","Sim");
+        cy.validateToast('Sucesso','Transação atualizada com sucesso');
+    });
+
+    it('Should delete selected transaction', ()=>{
+        cy.visit('/expenses')
+        cy.get('[type="radio"]').first().check();
+        cy.getByTestId("delete-button").click();
+        cy.validateToast('Sucesso','Transação deletada com sucesso')
+        cy.getByTestId('transaction-name').should("not.exist");
     })
 })
